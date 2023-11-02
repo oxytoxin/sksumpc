8b6dd3dab91fc08692b87caa7b3cf7704afa944e
oxytoxin <oxytoxinsgrace@gmail.com>
2023-11-03 02:28:34 +0800

    punchlist 10/24

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,1 @@
-import SimpleMaskMoney from 'simple-mask-money';
 import './bootstrap';
-import Currency from '@tadashi/currency';
-
-window.SimpleMaskMoney = SimpleMaskMoney;
-window.Currency = Currency;
