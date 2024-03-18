beb4e55430e6535c2da7bfd1cf2bdc022141cd3d
Mark John Lerry Casero <oxytoxinsgrace@gmail.com>
2024-03-19 00:10:59 +0800

    import commodity loans

--- database/seeders/ImportExistingLoansSeeder.php
+++ database/seeders/ImportExistingLoansSeeder.php
@@ -7,6 +7,7 @@ use App\Models\LoanType;
 use App\Models\Member;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\SimpleExcel\SimpleExcelReader;
 
 class ImportExistingLoansSeeder extends Seeder
 {
@@ -16,32 +17,19 @@ class ImportExistingLoansSeeder extends Seeder
     public function run(): void
     {
         $loanType = LoanType::find(4);
-        app(ImportExistingLoan::class)->handle(
-            member: Member::find(4),
-            loanType: $loanType,
-            reference_number: 1,
-            amount: 150000,
-            balance_forwarded: 94658.56,
-            number_of_terms: 48,
-            application_date: '11/10/2021',
-        );
-        app(ImportExistingLoan::class)->handle(
-            member: Member::find(7),
-            loanType: $loanType,
-            reference_number: 1,
-            amount: 150000,
-            balance_forwarded: 68902.67,
-            number_of_terms: 48,
-            application_date: '7/17/2021',
-        );
-        app(ImportExistingLoan::class)->handle(
-            member: Member::find(8),
-            loanType: $loanType,
-            reference_number: 1,
-            amount: 150000,
-            balance_forwarded: 71377.56,
-            number_of_terms: 48,
-            application_date: '7/17/2021',
-        );
+        $reader = SimpleExcelReader::create(storage_path('csv/loans/COMMODITY-LOAN.xlsx'), 'xlsx');
+        $members_code = $reader->getRows()->pluck('MEMBERS ID');
+        $members = Member::whereIn('mpc_code', $members_code->all())->get();
+        $reader->getRows()->each(function ($data) use ($members, $loanType) {
+            app(ImportExistingLoan::class)->handle(
+                member: $members->firstWhere('mpc_code', $data['MEMBERS ID']),
+                loanType: $loanType,
+                reference_number: $data['REFERENCE NUMBER'],
+                amount: $data['DESIRED AMOUNT'],
+                balance_forwarded: $data['BALANCE TO FORWARD'],
+                number_of_terms: $data['NUMBER OF TERMS'],
+                application_date: $data['DATE APPLIED'],
+            );
+        });
     }
 }
