d8755360bf78ac32ea01ec1151a3f0c7ea88c906
Mark John Lerry Casero <oxytoxinsgrace@gmail.com>
2024-03-20 22:13:26 +0800

    import loans

--- database/seeders/ImportExistingLoansSeeder.php
+++ database/seeders/ImportExistingLoansSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Actions\Loans\ImportExistingLoan;
 use App\Models\LoanType;
 use App\Models\Member;
+use DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\SimpleExcel\SimpleExcelReader;
@@ -16,20 +17,38 @@ class ImportExistingLoansSeeder extends Seeder
      */
     public function run(): void
     {
-        $loanType = LoanType::find(4);
-        $reader = SimpleExcelReader::create(storage_path('csv/loans/COMMODITY-LOAN.xlsx'), 'xlsx');
+        DB::beginTransaction();
+        $this->importLoansFromFile('REGULAR-LOAN.xlsx', LoanType::find(1));
+        $this->importLoansFromFile('EMERGENCY-LOAN.xlsx', LoanType::find(2));
+        $this->importLoansFromFile('PROVIDENTIAL-LOAN.xlsx', LoanType::find(3));
+        $this->importLoansFromFile('COMMODITY-LOAN.xlsx', LoanType::find(4));
+        $this->importLoansFromFile('KABUHAYAN-LOAN.xlsx', LoanType::find(5));
+        $this->importLoansFromFile('SPECIAL-LOAN.xlsx', LoanType::find(6));
+        $this->importLoansFromFile('FLY-NOW-PAY-LATER-LOAN.xlsx', LoanType::find(7));
+        $this->importLoansFromFile('RES-REGULAR-LOAN.xlsx', LoanType::find(11));
+        DB::commit();
+    }
+
+    protected function importLoansFromFile(string $filename, LoanType $loanType)
+    {
+        $reader = SimpleExcelReader::create(storage_path('csv/loans/' . $filename), 'xlsx');
         $members_code = $reader->getRows()->pluck('MEMBERS ID');
         $members = Member::whereIn('mpc_code', $members_code->all())->get();
         $reader->getRows()->each(function ($data) use ($members, $loanType) {
-            app(ImportExistingLoan::class)->handle(
-                member: $members->firstWhere('mpc_code', $data['MEMBERS ID']),
-                loanType: $loanType,
-                reference_number: $data['REFERENCE NUMBER'],
-                amount: $data['DESIRED AMOUNT'],
-                balance_forwarded: $data['BALANCE TO FORWARD'],
-                number_of_terms: $data['NUMBER OF TERMS'],
-                application_date: $data['DATE APPLIED'],
-            );
+            try {
+                if (filled($data['MEMBERS ID']))
+                    app(ImportExistingLoan::class)->handle(
+                        member: $members->firstWhere('mpc_code', $data['MEMBERS ID']),
+                        loanType: $loanType,
+                        reference_number: $data['REFERENCE NUMBER'],
+                        amount: $data['DESIRED AMOUNT'],
+                        balance_forwarded: $data['BALANCE TO FORWARD'],
+                        number_of_terms: $data['NUMBER OF TERMS'],
+                        application_date: $data['DATE APPLIED'],
+                    );
+            } catch (\Throwable $th) {
+                dd($data, $th);
+            }
         });
     }
 }
