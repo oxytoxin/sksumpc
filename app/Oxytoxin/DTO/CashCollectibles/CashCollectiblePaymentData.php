347ed45c935070febaab69fcfa2cf31a3c99aa15
Mark John Lerry A. Casero <166525108+denebaltairvega@users.noreply.github.com>
2024-05-09 21:21:41 +0800

    CashCollectiblePaymentData member_id should be nullable

--- app/Oxytoxin/DTO/CashCollectibles/CashCollectiblePaymentData.php
+++ app/Oxytoxin/DTO/CashCollectibles/CashCollectiblePaymentData.php
@@ -7,7 +7,7 @@ use Spatie\LaravelData\Data;
 class CashCollectiblePaymentData extends Data
 {
     public function __construct(
-        public int $member_id,
+        public ?int $member_id,
         public int $payment_type_id,
         public string $reference_number,
         public string $amount,
