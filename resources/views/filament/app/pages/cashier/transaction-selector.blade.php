18957db8c179e4293dddfcb942e8a4fcd5c1e053
Mark John Lerry A. Casero <166525108+denebaltairvega@users.noreply.github.com>
2024-07-12 09:45:25 +0800

    cashier transactions page add loan option and color

--- resources/views/filament/app/pages/cashier/transaction-selector.blade.php
+++ resources/views/filament/app/pages/cashier/transaction-selector.blade.php
@@ -6,39 +6,44 @@
         <ul class="ml-2 space-y-2">
             @if($member)
                 <li>
-                    <button wire:click="addTransaction('cbu')" class="text-left bg-gray-100 py-2 px-4 rounded-lg w-full cursor-pointer shadow font-semibold">
+                    <button data-transaction="cbu" wire:click="addTransaction('cbu')" class="text-left py-2 px-4 rounded-lg w-full cursor-pointer shadow font-semibold">
                         CBU
                     </button>
                 </li>
                 <li>
                     <details class="mb-2">
-                        <summary class="text-left bg-gray-100 py-2 px-4 rounded-lg cursor-pointer shadow">
+                        <summary class="text-left bg-red-200 py-2 px-4 rounded-lg cursor-pointer shadow">
                             <span class="font-semibold">MSO</span>
                         </summary>
                         <ul class="bg-white p-4 space-y-2">
                             <li class="text-gray-800">
-                                <button wire:click="addTransaction('savings')" class="text-left bg-gray-100 py-2 px-4 rounded-lg w-full cursor-pointer shadow font-semibold">REGULAR SAVINGS</button>
+                                <button data-transaction="savings" wire:click="addTransaction('savings')" class="text-left py-2 px-4 rounded-lg w-full cursor-pointer shadow font-semibold">REGULAR SAVINGS</button>
                             </li>
                             <li class="text-gray-800">
-                                <button wire:click="addTransaction('imprest')" class="text-left bg-gray-100 py-2 px-4 rounded-lg w-full cursor-pointer shadow font-semibold">IMPREST SAVINGS</button>
+                                <button data-transaction="imprest" wire:click="addTransaction('imprest')" class="text-left py-2 px-4 rounded-lg w-full cursor-pointer shadow font-semibold">IMPREST SAVINGS</button>
                             </li>
                             <li class="text-gray-800">
-                                <button wire:click="addTransaction('love_gift')" class="text-left bg-gray-100 py-2 px-4 rounded-lg w-full cursor-pointer shadow font-semibold">LOVE GIFT</button>
+                                <button data-transaction="love-gift" wire:click="addTransaction('love_gift')" class="text-left py-2 px-4 rounded-lg w-full cursor-pointer shadow font-semibold">LOVE GIFT</button>
                             </li>
                             <li class="text-gray-800">
-                                <button wire:click="addTransaction('time_deposit')" class="text-left bg-gray-100 py-2 px-4 rounded-lg w-full cursor-pointer shadow font-semibold">TIME DEPOSIT</button>
+                                <button data-transaction="time-deposit" wire:click="addTransaction('time_deposit')" class="text-left py-2 px-4 rounded-lg w-full cursor-pointer shadow font-semibold">TIME DEPOSIT</button>
                             </li>
                         </ul>
                     </details>
                 </li>
+                <li>
+                    <button data-transaction="loan" wire:click="addTransaction('loan')" class="text-left py-2 px-4 rounded-lg w-full cursor-pointer shadow font-semibold">
+                        LOAN
+                    </button>
+                </li>
             @endif
             <li>
-                <button wire:click="addTransaction('cash_collection')" class="text-left bg-gray-100 py-2 px-4 rounded-lg w-full cursor-pointer shadow font-semibold">
+                <button data-transaction="cash-collection" wire:click="addTransaction('cash_collection')" class="text-left py-2 px-4 rounded-lg w-full cursor-pointer shadow font-semibold">
                     CASH COLLECTIBLES
                 </button>
             </li>
             <li>
-                <button wire:click="addTransaction('others')" class="text-left bg-gray-100 py-2 px-4 rounded-lg w-full cursor-pointer shadow font-semibold">
+                <button data-transaction="others" wire:click="addTransaction('others')" class="text-left py-2 px-4 rounded-lg w-full cursor-pointer shadow font-semibold">
                     OTHERS
                 </button>
             </li>
