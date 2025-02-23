3ac5162ac87428cdad747cb58b3965b5573d2c33
oxytoxin <oxytoxinsgrace@gmail.com>
2025-02-24 03:14:48 +0800

    2/24

--- resources/views/livewire/bookkeeper-transaction-date-checker.blade.php
+++ resources/views/livewire/bookkeeper-transaction-date-checker.blade.php
@@ -1,6 +1,6 @@
 <div>
     @if (!config('app.transaction_date') && \Route::currentRouteName() != 'filament.app.pages.transaction-date-manager' && \Route::currentRouteName() != 'filament.app.pages.transaction-date-manager')
-    <div x-data x-init="$el.parentNode.nextElementSibling.remove()" class="absolute inset-0 z-[5] border-2 border-red-600 bg-white p-4">
+    <div x-data x-init="$el.parentNode.nextElementSibling.remove()" class="absolute inset-0 z-[9] border-2 border-red-600 bg-white p-4">
         <h3>No transaction date set by bookkeeper. Please coordinate with bookkeeper to set the transaction date.</h3>
     </div>
     @endif
